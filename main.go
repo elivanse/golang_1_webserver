3970e22a5824a62bb1f47cb488e65462bcc19808
IvanMaximilianoPereyra <elivanse@hotmail.com>
2022-05-18 23:27:02 -0300

    fixed

--- main.go
+++ main.go
@@ -15,30 +15,32 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Post request succesfulll")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
-	fmt.Fprintf(w, "name %s\n", name)
-	fmt.Fprintf(w, "address %s\n", address)
+	fmt.Fprintf(w, "Name %s\n", name)
+	fmt.Fprintf(w, "Address %s\n", address)
 
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found", http.StatusNotFound)
+		http.Error(w, "404 notfaund", http.StatusNotFound)
 		return
 	}
 	if r.Method != "GET" {
-		http.Error(w, "method is not supprted", http.StatusNotFound)
+		http.Error(w, "Metodo no soportado ...", http.StatusNotFound)
 		return
 	}
-	fmt.Fprintf(w, "hello!")
+	fmt.Fprintf(w, "hola!")
 }
 
 func main() {
-	fileServer := http.fileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
+	staticDir := http.Dir("./static")
+
+	staticfileServer := http.StripPrefix("/static/", http.FileServer(staticDir))
+	http.Handle("/", staticfileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", formHandler)
 
-	fmt.Printf("Iniciando server en el puerto 8080\n")
+	fmt.Printf("Iniciando server en el puerto 8080 ( ochenta ochenta ) \n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
